0c46caf9cb36d39dedc5f055ba23734b32be7873
nju33 <nju33.ki@gmail.com>
2018-06-28 11:58:08 +0900

    chore: add ci, fix: d.ts

--- src/index.test.ts
+++
@@ -1,3 +0,0 @@
-import togettheme = require('.');
-
-const theme = togettheme({});
--- togettheme.d.ts
+++ togettheme.d.ts
@@ -1,4 +1,14 @@
 import togetprop = require('togetprop');
 
-declare function togettheme(defaultValue?: togetprop.DefaultValues): togetprop.CreatePropPathTag;
-export = togettheme;
+declare module 'togettheme' {
+  type CreatePropPathTag = ReturnType<typeof togetprop>;
+  type DefaultValues = typeof togetprop extends (
+    _: any,
+    defaultValue: infer R
+  ) => CreatePropPathTag
+    ? R
+    : never;
+
+  function togettheme(defaultValue?: DefaultValues): CreatePropPathTag;
+  export = togettheme;
+}
